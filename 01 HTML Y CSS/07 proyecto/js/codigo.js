34b223db5200cf310732e907a2983fbbb1533bbc
Eduardo <dj.shadowjeab@gmail.com>
2024-03-11 22:40:30 -0500

    fin escritorio

---
+++ 01 HTML Y CSS/07 proyecto/js/codigo.js
@@ -0,0 +1,15 @@
+// JS MANIPULA EL DOM
+// DOCUMENT OBJECT MODEL -> HTML COMO UN OBJETO
+
+// console.log(document)
+// var
+// let y const
+// console.log(window.document);
+let nav = document.querySelector('.nav');
+
+console.log(nav);
+
+window.addEventListener('scroll', function(){
+    // console.log('hiciste scroll');
+    console.log(scrollY);
+})
