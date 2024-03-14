7971728afb94abd928e37b9f9f3e8d290c1a0ae0
Eduardo <dj.shadowjeab@gmail.com>
2024-03-13 21:11:24 -0500

    res

--- 01 HTML Y CSS/07 proyecto/js/codigo.js
+++ 01 HTML Y CSS/07 proyecto/js/codigo.js
@@ -5,11 +5,22 @@
 // var
 // let y const
 // console.log(window.document);
-let nav = document.querySelector('.nav');
+// let nav = document.querySelector('.nav');
+let welcomeNav = document.querySelector('.nav__welcome');
+let searchNav = document.querySelector('.nav__search');
 
-console.log(nav);
+// console.log(nav);
 
 window.addEventListener('scroll', function(){
     // console.log('hiciste scroll');
-    console.log(scrollY);
+    // console.log(scrollY);
+    if (scrollY > 0){
+        // console.log('Es mayor a cero');
+        welcomeNav.classList.add('d-hide');
+        searchNav.classList.add('d-hide');
+    } else {
+        // console.log('regresaste al punto cero');
+        welcomeNav.classList.remove('d-hide');
+        searchNav.classList.remove('d-hide');
+    }
 })
